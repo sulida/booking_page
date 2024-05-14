013da391f52f88a8a5ebe6c586f0f1c49ebe5618
sulida <sulida2011@gmail.com>
2024-05-14 11:58:23 +0300

    Initial commit

---
+++ src/main/java/project/ChromeRegistry.java
@@ -0,0 +1,17 @@
+package project;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeRegistry {
+
+    private static final String FILE_SEPARATOR = System.getProperty("file.separator");
+    private static final String PATH = "." +  FILE_SEPARATOR +  "src" +  FILE_SEPARATOR + "main" +  FILE_SEPARATOR
+            + "resources" +  FILE_SEPARATOR + "chromedriver_win64" +  FILE_SEPARATOR +  "chromedriver.exe";
+
+    public ChromeDriver registerDriver(){
+        System.setProperty("webdriver.chrome.driver", PATH);
+        return new ChromeDriver();
+    }
+
+
+}
